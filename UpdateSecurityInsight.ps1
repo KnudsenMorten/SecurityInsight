82f1f837c3edf8c6cf2a5ba48db52599d42de0f6
Morten Knudsen <121301838+KnudsenMorten@users.noreply.github.com>
2025-12-30 16:27:46 +0100

    cfg

---
+++ UpdateSecurityInsight.ps1
@@ -0,0 +1,28 @@
+﻿#########################################################################################################
+# Downloading latest version of SecurityInsight files
+#########################################################################################################
+
+$global:GitHubUri = "https://raw.githubusercontent.com/KnudsenMorten/SecurityInsight/main"
+$Files = @("SecurityInsight.ps1",`
+           "SecurityInsight_RiskAnalysis.yaml",`
+           "SecurityInsight_RiskIndex.csv",`
+           "CriticalAssetTagging.ps1",`
+           "CriticalAssetTagging.yaml",`
+           "RunCriticalAssetTagging.ps1",`
+           "RunSecurityInsight.ps1",`
+           "CriticalAssetTaggingMaintenance.ps1" `
+           )
+
+Write-Host "SecurityInsight"
+Write-Host "Created by Morten Knudsen, Microsoft MVP (@knudsenmortendk - mok@mortenknudsen.net)"
+Write-Host ""
+Write-Host "Downloading latest version of SecurityInsight from"
+Write-Host "$GitHubUri"
+Write-Host ""
+
+foreach ($File in $Files) {
+    Write-host "Updating $File"
+    $FileFullPath = Join-Path $PSScriptRoot $File
+    Remove-Item $FileFullPath -ErrorAction SilentlyContinue
+    Invoke-WebRequest "$GitHubUri/$File" -OutFile $FileFullPath
+}
